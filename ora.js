47a34f742a68d1e64948b12bc35249e95a74111e
UjlakanZoltan <71312599+UjlakanZoltan@users.noreply.github.com>
2020-10-02 17:20:50 +0200

    HTML 80% kész
    Jani nézd már meg kérlek, hogy a navbar miért nem működik. Tettem bele egy órát, ha feleslegesnek találod, akkor majd kiveszem. Holnap gyűjtök anyagot, hogy rendes szöveg legyen benne és ki kellene találni, hogy a kártyákra mi kerüljön. Mert szerintem így önmagában felesleges.

---
+++ ora.js
@@ -0,0 +1,20 @@
+setInterval(OraKiir,1);
+
+function OraKiir()
+{
+    var ido=new Date();
+    var ora=ido.getHours();
+    if (ora<10)
+        ora="0"+ora;
+    var perc=ido.getMinutes();
+    if (perc<10)
+            perc="0"+perc;
+    var mp=ido.getSeconds();
+    {
+        document.getElementById("ora").style.color="white";
+        document.getElementById("ora").style.fontSize="32px";       
+    }
+    var ezrdemp=ido.getMilliseconds();
+    var uj=ora+":"+perc+":"+mp;
+    document.getElementById("ora").innerHTML=uj;
+}
