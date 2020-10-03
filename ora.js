ce41172f96525484089658b769ab558396589259
janos03 <janosmikaczo@gmail.com>
2020-10-03 12:45:16 +0200

    Képek, a háttérhez, ikon, javítás
    Megcsináltam a menüt, mostmár normálisan gördül le-fel, színeket állítottam, és 1-2 dolgot megcsináltam. A hamburger menü még mindig nem működik, oka ismeretlek...

--- ora.js
+++ ora.js
@@ -1,20 +1,18 @@
-setInterval(OraKiir,1);
+setInterval(OraKiir, 1);
 
-function OraKiir()
-{
-    var ido=new Date();
-    var ora=ido.getHours();
-    if (ora<10)
-        ora="0"+ora;
-    var perc=ido.getMinutes();
-    if (perc<10)
-            perc="0"+perc;
-    var mp=ido.getSeconds();
-    {
-        document.getElementById("ora").style.color="white";
-        document.getElementById("ora").style.fontSize="32px";       
+function OraKiir() {
+    var ido = new Date();
+    var ora = ido.getHours();
+    if (ora < 10)
+        ora = "0" + ora;
+    var perc = ido.getMinutes();
+    if (perc < 10)
+        perc = "0" + perc;
+    var mp = ido.getSeconds(); {
+        document.getElementById("ora").style.color = "#1BA098";
+        document.getElementById("ora").style.fontSize = "32px";
     }
-    var ezrdemp=ido.getMilliseconds();
-    var uj=ora+":"+perc+":"+mp;
-    document.getElementById("ora").innerHTML=uj;
+    var ezrdemp = ido.getMilliseconds();
+    var uj = ora + ":" + perc + ":" + mp;
+    document.getElementById("ora").innerHTML = uj;
 }
