f2b222b1055819962e379adba417ba1c7907951e
janos03 <janosmikaczo@gmail.com>
2020-10-06 11:03:27 +0200

    Zene belerakva, Kátyák animációja is görgetésnél
    Még a zenekezelő nincs teljesen kész, a ki be kapcsolása megvan, de a magasság állítása, és  ilyenek nem szuperálnak sajnos :(

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ $(window).scroll(function() {
         $(nav).removeClass('fixed');
     }
 })
+var audio = document.getElementById("audio")
+
+function zene() {
+    var x = document.getElementById("canvas");
+    if (x.style.display === "none") {
+        x.style.display = "block"
+        audio.play()
+
+
+    } else {
+        x.style.display = "none"
+        audio.pause();
+    }
+
+}
+
+function updateSlider() {
+
+    var x = document.getElementById("magassag").value
+    document.getElementById("canvas").style.height = "500px";
+    document.getElementById("canvas").style.top = "90%"
+}
